d85e52418e84620e5e00e422fc987eafab0ec2bb
AK101111 <arnavkansal@gmail.com>
2016-03-26 02:29:19 +0530

    add error check on connection

--- api/connect_db.php
+++ api/connect_db.php
@@ -1,3 +1,6 @@
 <?php
 	$connection = new mysqli("localhost","root","mysqlpass","CMS");
+	if ($connection->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error;
+	}
 ?>
